4a0646947ef1897aad7cda8a8ddea39afaf07c1b
rahadityaputra <rahadit.a.p123@gmail.com>
2024-07-19 06:14:13 +0700

    mengganti model

--- script.js
+++
@@ -1,5 +0,0 @@
-const menuToggle = document.querySelector("nav .menu-toggle");
-const menu = document.querySelector("nav .menu");
-menuToggle.addEventListener("click", function () {
-	menu.classList.toggle("animasi");
-});
